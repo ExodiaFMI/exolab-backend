0d49d4a4ed85aa2cabb63eeb7ab0490c21a60af4
Konstantin Nikolov <kosi691@icloud.com>
2025-03-15 01:42:11 +0200

    Add SubjectName enum and update Subject entity to use enum for name field

---
+++ src/enums/subject.enum.ts
@@ -0,0 +1,9 @@
+export enum SubjectName {
+    MATH = "Math",
+    PHYSICS = "Physics",
+    CHEMISTRY = "Chemistry",
+    BIOLOGY = "Biology",
+    COMPUTER_SCIENCE = "Computer Science",
+    HISTORY = "History",
+    LITERATURE = "Literature"
+}
--- src/modules/subject/subject.entity.ts
+++ src/modules/subject/subject.entity.ts
@@ -1,14 +1,15 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { SubjectName } from "../../enums/subject.enum";
 
 @Entity("subjects")
 export class Subject {
     @PrimaryGeneratedColumn()
     id!: number;
 
-    @Column({ unique: true })
-    name: string;
+    @Column({ type: "enum", enum: SubjectName, unique: true })
+    name: SubjectName;
 
-    constructor(name: string) {
+    constructor(name: SubjectName) {
         this.name = name;
     }
 }
