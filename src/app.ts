7f9f23e1a432899e7eba1719a9e564a00b4c0333
Konstantin Nikolov <kosi691@icloud.com>
2025-03-15 21:49:41 +0200

    Enhance express app configuration with body-parser and CORS settings

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import { useExpressServer } from 'routing-controllers';
 import { setupSwagger } from './config/swagger';
 import { UserController } from './modules/user/user.controller';
@@ -10,16 +11,27 @@ import { CourseController } from './modules/course/course.controller';
 import { TopicController } from './modules/topic/topic.controller';
 import { SubtopicController } from './modules/subtopic/subtopic.controller';
 import { SubjectController } from './modules/subject/subject.controller';
-import { AgentController } from './modules/agent/agent.controller'; 
-
+import { AgentController } from './modules/agent/agent.controller';
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.raw({ limit: '100mb' }));
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 86400,
+}));
+
 setupSwagger(app);
-app.use(cors());
-app.use(express.json());
 
 useExpressServer(app, {
   controllers: [
